119c7c78f96d12f93b789a64db3c3e1319b6c8ed
Lilit Hakobyan <lilit.hakobyan@helpsystems.com>
2018-08-20 14:37:56 +0400

    change game name

---
+++ game/src/AuthorQuiz.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './App.css';
+import './bootstrap.min.css';
+
+class AuthorQuiz extends Component {
+  render() {
+    return (
+     <div>Author Quiz</div>
+    );
+  }
+}
+
+export default AuthorQuiz;
--- game/src/index.js
+++ game/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import AuthorQuiz from './AuthorQuiz';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<AuthorQuiz />, document.getElementById('root'));
 registerServiceWorker();
