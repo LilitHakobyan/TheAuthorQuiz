182207beb8a6463a5091b88d8e7b3b107a265c64
Lilit Hakobyan <lilit.hakobyan@helpsystems.com>
2018-08-22 16:44:44 +0400

    bpm

--- game/src/AuthorQuiz.js
+++ game/src/AuthorQuiz.js
@@ -2,10 +2,38 @@ import React, { Component } from 'react';
 import './App.css';
 import './bootstrap.min.css';
 
+function Hero () {
+  return (<div className="row">
+  <div className="jumbotron col-10 offset-1">
+  <h1>Author Quiz</h1>
+  <p>Select the book written by the author shown</p>
+   </div>
+  </div>);
+  }
+
+  function Turn () {
+    return(<div/>);
+  }
+
+  function Continue () {
+    return(<div/>);
+  }
+  function Footer () {
+    return(<div id="footer" className="row">
+         <div className="col-12">
+         <p className="text-muted credit"> All images are from <a href="https://en.wikipedia.org/wiki/Main_Page" >Wiki</a></p>
+           </div>   
+      </div>);
+  }
 class AuthorQuiz extends Component {
   render() {
     return (
-     <div>Author Quiz</div>
+     <div className="container-fluid">
+              <Hero />
+              <Turn />
+              <Continue />
+              <Footer />
+     </div>
     );
   }
 }
