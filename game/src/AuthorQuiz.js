a4ace338d15f159653a37dece84e8622e8f6374a
Lilit Hakobyan <lilit.hakobyan@helpsystems.com>
2018-08-23 23:28:11 +0400

    validation

--- game/src/AuthorQuiz.js
+++ game/src/AuthorQuiz.js
@@ -1,4 +1,5 @@
-import React, { Component } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import './App.css';
 import './bootstrap.min.css';
 
@@ -34,6 +35,20 @@ function Hero () {
          </div>
     </div>);
   }
+
+  Turn.propTypes ={
+   author: PropTypes.shape(
+     {
+       name: PropTypes.string.isRequired,
+       imageUrl :PropTypes.string.isRequired,
+       imageSource :PropTypes.string.isRequired,
+       books: PropTypes.arrayOf(PropTypes.string).isRequired
+     }
+   ),
+   books: PropTypes.arrayOf(PropTypes.string).isRequired,
+   onAnswerSelected:PropTypes.func.isRequired,
+   highlight:PropTypes.string.isRequired
+  };
 
   function Continue () {
     return(<div/>);
